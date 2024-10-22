5ebdbc1206998e865a80e5f246527d69e2527b73
SaraErbismann <85120519+SaraErbismann@users.noreply.github.com>
2024-10-22 14:38:48 +0300

    Add simple navbar and create files for screens

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import StatisticsScreen from './statisticsScreen';
+import SettingsScreen from './statisticsScreen';
+import TimerScreen from './timerScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='Statistics' component={StatisticsScreen} />
+        <Tab.Screen name='Timer' component={TimerScreen} />
+        <Tab.Screen name='Settings' component={SettingsScreen} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
