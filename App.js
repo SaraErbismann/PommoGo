0608c05121e082f80fd8dad57cad9a04b76c0ecc
SaraErbismann <85120519+SaraErbismann@users.noreply.github.com>
2024-10-22 17:11:14 +0300

    Add simple navbar and create files for screens (#5)
    * Add simple navbar and create files for screens
    
    * Add icons to navbar
    
    * Set up timer with initial value

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import StatisticsScreen from './statisticsScreen';
+import SettingsScreen from './statisticsScreen';
+import TimerScreen from './timerScreen';
+import Ionicons from '@expo/vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Settings') {
+              iconName = 'settings'
+            } else if (route.name === 'Timer') {
+              iconName = 'timer'
+            } else {
+              iconName = 'stats-chart'
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name='Statistics' component={StatisticsScreen} />
+        <Tab.Screen name='Timer' component={TimerScreen} />
+        <Tab.Screen name='Settings' component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
