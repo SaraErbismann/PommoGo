d99e2e8c6452972311861e0b289d3117b78c3822
SaraErbismann <85120519+SaraErbismann@users.noreply.github.com>
2024-10-22 14:54:41 +0300

    Add icons to navbar

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import StatisticsScreen from './statisticsScreen';
 import SettingsScreen from './statisticsScreen';
 import TimerScreen from './timerScreen';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -13,11 +14,26 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Settings') {
+              iconName = 'settings'
+            } else if (route.name === 'Timer') {
+              iconName = 'timer'
+            } else {
+              iconName = 'stats-chart'
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
         <Tab.Screen name='Statistics' component={StatisticsScreen} />
         <Tab.Screen name='Timer' component={TimerScreen} />
         <Tab.Screen name='Settings' component={SettingsScreen} />
-
       </Tab.Navigator>
     </NavigationContainer>
 
